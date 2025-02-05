2c01be22418328424a41ea58f7bec7b311b977a5
fernando borel <fernandomenezesborel@gmail.com>
2025-02-05 15:32:11 -0300

    Feat. Adicionada estrutura do projeto.

---
+++ InfoDengue/Mappings/RelatorioMap.cs
@@ -0,0 +1,34 @@
+﻿using InfoDengue.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InfoDengue.Mappings;
+
+public class RelatorioMap : IEntityTypeConfiguration<Relatorio>
+{
+    public void Configure(EntityTypeBuilder<Relatorio> builder)
+    {
+        builder.HasKey(e => e.IdSolicitanteRelatorio).HasName("PK__Relatori__16E464897A64619E");
+
+        builder.Property(e => e.Arbovirose)
+            .HasMaxLength(50)
+            .IsUnicode(false);
+        builder.Property(e => e.CodIbge).HasColumnName("Cod_Ibge");
+        builder.Property(e => e.CpfSolicitante)
+            .HasMaxLength(11)
+            .IsUnicode(false)
+            .IsFixedLength();
+        builder.Property(e => e.DataSolicitacao).HasColumnName("Data_Solicitacao");
+        builder.Property(e => e.Municipio)
+            .HasMaxLength(100)
+            .IsUnicode(false);
+        builder.Property(e => e.SemInicio).HasColumnName("Sem_Inicio");
+        builder.Property(e => e.SemTermino).HasColumnName("Sem_Termino");
+
+        builder.HasOne(d => d.CpfSolicitanteNavigation).WithMany(p => p.Relatorios)
+            .HasPrincipalKey(p => p.Cpf)
+            .HasForeignKey(d => d.CpfSolicitante)
+            .OnDelete(DeleteBehavior.ClientSetNull)
+            .HasConstraintName("FK__Relatorio__CpfSo__398D8EEE");
+    }
+}
---
+++ InfoDengue/Repositories/SolicitanteRepository.cs
@@ -0,0 +1,28 @@
+﻿using InfoDengue.Context;
+using InfoDengue.Entities;
+using InfoDengue.Interfaces.Repositories;
+
+namespace InfoDengue.Repositories;
+
+public class SolicitanteRepository : BaseRepository<Solicitante>, ISolicitanteRepository
+{
+    private readonly InfoDengueDbContext _context;
+
+    public SolicitanteRepository(InfoDengueDbContext context) : base(context)
+      => _context = context;
+
+    public Solicitante GetCpfAsync(string cpf)
+    {
+        var cpfSolicitante = _context.Solicitantes.FirstOrDefault(s => s.Cpf == cpf);
+        return cpfSolicitante;
+    }
+
+    override public void Create(Solicitante entity)
+    {
+        var cpfSolicitante = GetCpfAsync(entity.Cpf);
+        if(cpfSolicitante != null)
+            throw new Exception("CPF já cadastrado");
+
+        base.Create(entity);
+    }
+}
